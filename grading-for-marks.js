a6073064510a9ecb2a65b7251e9df9b744d3185a
webgurusam <shyamghosh666@gmail.com>
2022-02-07 17:32:39 +0600

    SSC result marks grading system

--- grading-for-marks.js
+++ grading-for-marks.js
@@ -1,12 +1,24 @@
-
 // Grading for marks
-
-
-
-
-
-
-
+function getMarks(inputMarks) {
+    if (inputMarks >= 80 && inputMarks <= 100) {
+        return 'A+';
+    } else if (inputMarks <= 79 && inputMarks >= 70) {
+        return 'A';
+    } else if (inputMarks >= 60 && inputMarks <= 69) {
+        return 'A-';
+    } else if (inputMarks >= 50 && inputMarks <= 59) {
+        return 'B';
+    } else if (inputMarks >= 40 && inputMarks <= 49) {
+        return 'C';
+    } else if (inputMarks >= 33 && inputMarks <= 39) {
+        return 'D';
+    } else {
+        return 'F';
+    }
+}
+const inputMarks = 100;
+const marksGrading = getMarks(inputMarks);
+console.log('You got in SSC', marksGrading, 'grade result.');
 
 
 
